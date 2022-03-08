8e40c62ed69a0330f32ffdfe49b518fdea264031
Shiv0412 <shivomthakur38@gmail.com>
2022-03-08 12:04:40 +0530

    New chnages on 08-03-2022

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <p>hello world</p>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Menu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+let Menu = () =>{
+    return(
+        <div>
+
+        </div>
+    );
+}
+export default Menu;
