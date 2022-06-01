6488922ec462029ad712dd384ed43b43f8e17ee6
Shiv0412 <shivomthakur38@gmail.com>
2022-06-01 15:48:53 +0530

    01-01-2022

--- src/Redux/reducers/dataReducer.ts
+++ src/Redux/reducers/dataReducer.ts
@@ -1,37 +1,41 @@
 import { initialstate } from "./initialState";
 
-interface Action_Data{
-type:string;
-values: {
-  id:string,
-  name:string,
-  phone:Number,
-  DateOfBirth:Date,
-  Bloodgroup:string,
-  Gender:string,
-  City:string,
-  State:string,
-  Pincode: Number,
-  RegDate: string,
-  Address: string,
-  Bloodbank: string,
-  medical:string
-}; 
-id: string;
+interface Action_Data {
+  type: string;
+  values: {
+    id: string;
+    name: string;
+    phone: Number;
+    DateOfBirth: Date;
+    Bloodgroup: string;
+    Gender: string;
+    City: string;
+    State: string;
+    Pincode: Number;
+    RegDate: string;
+    Address: string;
+    Bloodbank: string;
+    medical: string;
+  };
+  id: string;
 }
 
-const dataReducer = (state = initialstate, action:Action_Data) => {
+const dataReducer = (state = initialstate, action: Action_Data) => {
   switch (action.type) {
     case "SAVEDATA":
       return [...state, { ...action.values, id: Date.now() }];
 
     case "REMOVE_DONAR":
-      return state.filter((values:Action_Data["values"]) => values.id !== action.id);
+      return state.filter(
+        (values: Action_Data["values"]) => values.id !== action.id
+      );
 
     case "UPDATE_DONAR":
       var data = state;
-      state = data.filter((values:Action_Data["values"]) => values.id !== action.values.id);
-      return [...state, { ...action.values}];
+      state = data.filter(
+        (values: Action_Data["values"]) => values.id !== action.values.id
+      );
+      return [...state, { ...action.values }];
 
     default:
       return state;
--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -2,6 +2,28 @@ import rootReducer from "./reducers";
 
 import { createStore } from "redux";
 
-const store = createStore(rootReducer);
+//const store = createStore(rootReducer);
 
-export default store;
+function saveToLocalStorage(state:any) {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+  function loadFromLocalStorage() {
+    try {
+      const serialisedState = localStorage.getItem("persistantState");
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+  const store = createStore(rootReducer, loadFromLocalStorage());
+  store.subscribe(() => saveToLocalStorage(store.getState()));
+  
+  export default store;
+  
